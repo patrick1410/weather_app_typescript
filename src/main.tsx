b022227a57ccaed3b9c0d2af4a465a12d707344e
Patrick <patrick_btc_96@hotmail.com>
2024-09-12 21:29:35 +0200

    added font

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Dashboard } from "./Dashboard.tsx";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Open Sans", Arial, Helvetica, sans-serif',
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Dashboard />
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   </StrictMode>
 );
